539fd6742e5b2402ea69d9483bbe03d4cb5469ff
Yashwani Kushwaha <yashwani45@gmail.com>
2026-05-30 22:40:13 +0530

    fix firebase admin env

--- Interviewplatform/server/config/firebaseAdmin.js
+++ Interviewplatform/server/config/firebaseAdmin.js
@@ -1,11 +1,22 @@
+// import admin from "firebase-admin";
+// import fs from "fs";
+
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync(new URL("./serverkey.json", import.meta.url))
+// );
+
+// // prevent re-initialization issue
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
+
+// export default admin;
 import admin from "firebase-admin";
-import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./serverkey.json", import.meta.url))
-);
+const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
 
-// prevent re-initialization issue
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
