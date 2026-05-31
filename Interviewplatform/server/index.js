c39459b647c3f37d6e90b36b4724b469368f0fcf
Yashwani Kushwaha <yashwani45@gmail.com>
2026-05-31 06:03:18 +0530

    fix cors issue

--- Interviewplatform/server/index.js
+++ Interviewplatform/server/index.js
@@ -11,9 +11,10 @@ dotenv.config();
 
 const app = express();
 
+
 const allowedOrigins = [
   "http://localhost:5173",
-  "interview-hub-t85e.vercel.app"
+  "https://interview-hub-t85e.vercel.app"  
 ];
 
 app.use(cors({
